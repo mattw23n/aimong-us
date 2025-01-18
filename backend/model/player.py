1b7067245b7fc2d06f1d5f9ebd07bebaa0000e3e
Deterjen <samuel.ysusanto@gmail.com>
2025-01-19 01:14:03 +0800

    Yes

--- backend/model/player.py
+++ backend/model/player.py
@@ -6,9 +6,3 @@ class Player(BaseModel):
     id: int
     name: str
     is_ai: bool = False
-
-class Session:
-    def __init__(self, session_id: int):
-        self.id = session_id
-        self.players: List[Player] = []
-        self.chat_log: List[dict] = []
